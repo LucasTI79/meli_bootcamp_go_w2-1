42e4a1921ebe9a2b8c4f8969da34aa4e8aa85a98
Lucas Melo <lucassss8v@gmail.com>
2023-06-12 11:23:46 -0300

    feat: add get-all endpoint for sellers

--- cmd/server/handler/seller.go
+++ cmd/server/handler/seller.go
@@ -1,21 +1,38 @@
 package handler
 
 import (
+	"net/http"
+
+	"github.com/extmatperez/meli_bootcamp_go_w2-1/internal/seller"
+	"github.com/extmatperez/meli_bootcamp_go_w2-1/pkg/web"
 	"github.com/gin-gonic/gin"
 )
 
 type Seller struct {
+	sellerService seller.Service
 	// sellerService seller.Service
 }
 
-func NewSeller() *Seller {
+func NewSeller(s seller.Service) *Seller {
 	return &Seller{
+		sellerService: s,
 		// sellerService: s,
 	}
 }
 
 func (s *Seller) GetAll() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		sellers, err := s.sellerService.GetAll(c.Request.Context())
+		if err != nil {
+			web.Error(c, http.StatusInternalServerError, "internal server error")
+			return
+		}
+		if len(sellers) == 0 {
+			web.Response(c, 204, sellers)
+			return
+		}
+		web.Success(c, http.StatusOK, sellers)
+	}
 }
 
 func (s *Seller) Get() gin.HandlerFunc {
--- internal/seller/service.go
+++ internal/seller/service.go
@@ -1,6 +1,7 @@
 package seller
 
 import (
+	"context"
 	"errors"
 
 	"github.com/extmatperez/meli_bootcamp_go_w2-1/internal/domain"
@@ -12,11 +13,17 @@ var (
 )
 
 type Service interface {
-	GetAll() ([]domain.Seller, error)
+	GetAll(ctx context.Context) ([]domain.Seller, error)
 }
 
-type service struct{}
+type service struct {
+	repository Repository
+}
 
-func NewService() Service {
-	return &service{}
+func (s *service) GetAll(ctx context.Context) ([]domain.Seller, error) {
+	return s.repository.GetAll(ctx)
+}
+
+func NewService(r Repository) Service {
+	return &service{repository: r}
 }
