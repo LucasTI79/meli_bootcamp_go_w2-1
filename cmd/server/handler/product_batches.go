2de12b83ded11f5d8fcc4cd33347ae99c3d8a1a7
Vitoria Raissa Zeferino <90735412+vraissa@users.noreply.github.com>
2023-07-09 17:30:55 -0300

    feat: Insertion of func in service of product

--- cmd/server/handler/product_batches.go
+++ cmd/server/handler/product_batches.go
@@ -4,47 +4,25 @@ import (
 	"net/http"
 
 	"github.com/extmatperez/meli_bootcamp_go_w2-1/internal/domain"
+	"github.com/extmatperez/meli_bootcamp_go_w2-1/internal/product"
 	"github.com/extmatperez/meli_bootcamp_go_w2-1/internal/product_batches"
-	"github.com/extmatperez/meli_bootcamp_go_w2-1/pkg/apperr"
+	"github.com/extmatperez/meli_bootcamp_go_w2-1/internal/section"
 	"github.com/extmatperez/meli_bootcamp_go_w2-1/pkg/web"
 	"github.com/gin-gonic/gin"
 )
 
 type ProductBatches struct {
-	service product_batches.Service
+	productBatchService product_batches.Service
+	sectionService      section.Service
+	productService      product.Service
 }
 
-type CreateProductBatchesRequest struct {
-	BatchNumber        string  `json:"batch_number"`
-	CurrentQuantity    int     `json:"current_quantity"`
-	CurrentTemperature float64 `json:"current_temperature"`
-	DueDate            string  `json:"due_date"`
-	InitialQuantity    int     `json:"initial_quantity"`
-	ManufacturingDate  string  `json:"manufacturing_date"`
-	ManufacturingHour  string  `json:"manufacturing_hour"`
-	MinimumTemperature int     `json:"minimum_temperature"`
-	ProductID          int     `json:"product_id"`
-	SectionID          int     `json:"section_id"`
-}
-
-func (s *CreateProductBatchesRequest) ToProductBatches() domain.ProductBatches {
-
-	return domain.ProductBatches{
-		BatchNumber:        s.BatchNumber,
-		CurrentQuantity:    s.CurrentQuantity,
-		CurrentTemperature: s.CurrentTemperature,
-		DueDate:            s.DueDate,
-		InitialQuantity:    s.InitialQuantity,
-		ManufacturingDate:  s.ManufacturingDate,
-		ManufacturingHour:  s.ManufacturingHour,
-		MinimumTemperature: s.MinimumTemperature,
-		ProductID:          s.ProductID,
-		SectionID:          s.SectionID,
+func NewProductBatches(pb product_batches.Service, sectionService section.Service, productService product.Service) *ProductBatches {
+	return &ProductBatches{
+		productBatchService: pb,
+		sectionService:      sectionService,
+		productService:      productService,
 	}
-}
-
-func NewProductBatches(service product_batches.Service) *ProductBatches {
-	return &ProductBatches{service}
 }
 
 // Create godoc
@@ -60,44 +38,19 @@ func NewProductBatches(service product_batches.Service) *ProductBatches {
 // @Failure 422 {object} web.ErrorResponse "Validation error"
 // @Failure 500 {object} web.ErrorResponse "Internal server error"
 // @Router /product-batches [post]
-func (pb *ProductBatches) Create() gin.HandlerFunc {
+func (s *ProductBatches) Create() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		request := c.MustGet(RequestParamContext).(CreateProductBatchesRequest)
-
-		created, err := pb.service.Create(request.ToProductBatches())
-
-		if err != nil {
-			if apperr.Is[*apperr.ResourceAlreadyExists](err) {
-				web.Error(c, http.StatusConflict, err.Error())
-				return
-			}
-			if apperr.Is[*apperr.DependentResourceNotFound](err) {
-				web.Error(c, http.StatusNotFound, err.Error())
-				return
-			}
+		var request domain.ProductBatches
+		if err := c.ShouldBindJSON(&request); err != nil {
+			web.Error(c, http.StatusBadRequest, err.Error())
+			return
 		}
-		web.Success(c, http.StatusCreated, created)
-	}
-}
 
-// Create godoc
-// @Summary Count product batches by section
-// @Description Count product batches by section
-// @Description Bring the number of product batches by section
-// @Tags PtoductBatches
-// @Accept json
-// @Produce json
-// @Success 200 {object} []domain.CountProductBatchesBySection "List of product batches by section"
-// @Failure 400 {object} web.ErrorResponse "Validation error"
-// @Failure 404 {object} web.ErrorResponse "Resource not found error"
-// @Failure 500 {object} web.ErrorResponse "Internal server error"
-// @Router /product-batches [get]
-func (pb *ProductBatches) Get() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		//id := c.Param("id")
+		if err := request.Validate(); err != nil {
+			web.Error(c, http.StatusUnprocessableEntity, err.Error())
+			return
+		}
 
-		productBatches := pb.service.CountProductBatchesBySection()
-
-		web.Success(c, http.StatusOK, productBatches)
+		err := s.productService.ValidateProductID(request.ProductID)
 	}
 }
