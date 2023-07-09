9d838a3dac09d471e6053160c096410b5efc12ce
Vitoria Raissa Zeferino <90735412+vraissa@users.noreply.github.com>
2023-07-09 02:17:21 -0300

    feat: created mock by service

--- internal/product_batches/mocks/service_mock.go
+++ internal/product_batches/mocks/service_mock.go
@@ -1,1 +1,31 @@
 package mocks
+
+import (
+	"github.com/extmatperez/meli_bootcamp_go_w2-1/internal/domain"
+	"github.com/stretchr/testify/mock"
+)
+
+type Service struct {
+	mock.Mock
+}
+
+func (s *Service) Exists(BatchNumber string) bool {
+	args := s.Called(BatchNumber)
+	return args.Get(0).(bool)
+}
+func (s *Service) CheckSectionExists(id int) bool {
+	args := s.Called(id)
+	return args.Get(0).(bool)
+}
+func (s *Service) CheckProductExists(id int) bool {
+	args := s.Called(id)
+	return args.Get(0).(bool)
+}
+func (s *Service) Create(pb domain.ProductBatches) int {
+	args := s.Called(pb)
+	return args.Get(0).(int)
+}
+func (s *Service) CountProductBatchesBySection() []domain.CountProductBatchesBySection {
+	args := s.Called()
+	return args.Get(0).([]domain.CountProductBatchesBySection)
+}
