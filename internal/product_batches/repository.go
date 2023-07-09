26d4eb490b4596d66048c58429a589a036b9e165
Vitoria Raissa Zeferino <90735412+vraissa@users.noreply.github.com>
2023-07-09 12:57:52 -0300

    fix: repository tweaks

--- internal/product_batches/repository.go
+++ internal/product_batches/repository.go
@@ -15,7 +15,6 @@ const (
 	CountProductBatchesBySectionQuery = "SELECT pb.section_id, s.section_number, COUNT(s.id) 'products_count' FROM product_batches pb JOIN products p ON p.id = pb.product_id JOIN sections s ON s.id = pb.section_id GROUP BY s.id"
 )
 
-// Repository encapsulates the storage of a product_batches.
 type Repository interface {
 	Save(pb domain.ProductBatches) int
 	Get(id int) *domain.ProductBatches
@@ -37,16 +36,12 @@ func NewRepository(db *sql.DB) Repository {
 
 func (r *repository) Save(pb domain.ProductBatches) int {
 
-	if !r.sectionExists(pb.SectionID) {
-		panic("section does not exist")
-	}
-
 	stmt, err := r.db.Prepare(InsertQuery)
 	if err != nil {
 		panic(err)
 	}
 
-	res, err := stmt.Exec(pb.BatchNumber)
+	res, err := stmt.Exec(pb.BatchNumber, pb.CurrentQuantity, pb.CurrentTemperature, pb.DueDate, pb.InitialQuantity, pb.ManufacturingDate, pb.ManufacturingHour, pb.MinimumTemperature, pb.ProductID, pb.SectionID)
 	if err != nil {
 		panic(err)
 	}
@@ -77,17 +72,6 @@ func (r *repository) Exists(BatchNumber string) bool {
 	row := r.db.QueryRow(ExistsQuery, BatchNumber)
 	err := row.Scan(&BatchNumber)
 	return err == nil
-}
-
-func (r *repository) sectionExists(id int) bool {
-
-	query := `SELECT id FROM sections WHERE id=?`
-	var count int
-	err := r.db.QueryRow(query, id).Scan(&count)
-	if err != nil {
-		panic(err)
-	}
-	return count > 0
 }
 
 func (r *repository) CheckSectionExists(id int) bool {
