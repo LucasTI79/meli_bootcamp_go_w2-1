c66ac7774762753750ba78d836dcd8519e0c2a6e
Davidson Bruno <davidsonbruno@outlook.com>
2023-07-08 21:23:28 -0300

    fix: removed unused context for product mocks

--- internal/product/mocks/service_mock.go
+++ internal/product/mocks/service_mock.go
@@ -1,8 +1,6 @@
 package mocks
 
 import (
-	"context"
-
 	"github.com/extmatperez/meli_bootcamp_go_w2-1/internal/domain"
 	"github.com/stretchr/testify/mock"
 )
@@ -11,27 +9,27 @@ type Service struct {
 	mock.Mock
 }
 
-func (s *Service) GetAll(c context.Context) []domain.Product {
+func (s *Service) GetAll() []domain.Product {
 	args := s.Called()
 	return args.Get(0).([]domain.Product)
 }
 
-func (s *Service) Get(c context.Context, id int) (*domain.Product, error) {
+func (s *Service) Get(id int) (*domain.Product, error) {
 	args := s.Called(id)
 	return args.Get(0).(*domain.Product), args.Error(1)
 }
 
-func (s *Service) Create(c context.Context, p domain.Product) (*domain.Product, error) {
+func (s *Service) Create(p domain.Product) (*domain.Product, error) {
 	args := s.Called(p)
 	return args.Get(0).(*domain.Product), args.Error(1)
 }
 
-func (s *Service) Update(c context.Context, id int, p domain.UpdateProduct) (*domain.Product, error) {
+func (s *Service) Update(id int, p domain.UpdateProduct) (*domain.Product, error) {
 	args := s.Called(id, p)
 	return args.Get(0).(*domain.Product), args.Error(1)
 }
 
-func (s *Service) Delete(c context.Context, id int) error {
+func (s *Service) Delete(id int) error {
 	args := s.Called(id)
 	return args.Error(0)
 }
