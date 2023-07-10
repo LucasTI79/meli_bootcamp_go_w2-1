84f43be6a8f32a7f4a7cedf23bfcb7c292010fac
Rafael Bonin <rafalyf11@gmail.com>
2023-07-10 10:08:29 -0300

    feat: inbound order service and repository mocks

---
+++ internal/inbound_orders/mocks/repository_mock.go
@@ -0,0 +1,25 @@
+package mocks
+
+import (
+	"github.com/extmatperez/meli_bootcamp_go_w2-1/internal/domain"
+	"github.com/stretchr/testify/mock"
+)
+
+type Repository struct {
+	mock.Mock
+}
+
+func (r *Repository) Get(id int) *domain.InboundOrder {
+	args := r.Called(id)
+	return args.Get(0).(*domain.InboundOrder)
+}
+
+func (r *Repository) Save(i domain.InboundOrder) int {
+	args := r.Called(i)
+	return args.Get(0).(int)
+}
+
+func (r *Repository) Exists(orderNumber int) bool {
+	args := r.Called(orderNumber)
+	return args.Get(0).(bool)
+}
