6c79ff697fbfcc854eca46c02fee3cf28d331a6f
Rafael Bonin <rafalyf11@gmail.com>
2023-07-10 10:10:12 -0300

    feat: removed context and added new employee mock methods

--- internal/employee/mocks/service_mock.go
+++ internal/employee/mocks/service_mock.go
@@ -1,8 +1,6 @@
 package mocks
 
 import (
-	"context"
-
 	"github.com/extmatperez/meli_bootcamp_go_w2-1/internal/domain"
 	"github.com/stretchr/testify/mock"
 )
@@ -11,27 +9,37 @@ type Service struct {
 	mock.Mock
 }
 
-func (s *Service) GetAll(c context.Context) []domain.Employee {
+func (s *Service) GetAll() []domain.Employee {
 	args := s.Called()
 	return args.Get(0).([]domain.Employee)
 }
 
-func (s *Service) Get(c context.Context, id int) (*domain.Employee, error) {
+func (s *Service) Get(id int) (*domain.Employee, error) {
 	args := s.Called(id)
 	return args.Get(0).(*domain.Employee), args.Error(1)
 }
 
-func (s *Service) Create(c context.Context, p domain.Employee) (*domain.Employee, error) {
+func (s *Service) Create(p domain.Employee) (*domain.Employee, error) {
 	args := s.Called(p)
 	return args.Get(0).(*domain.Employee), args.Error(1)
 }
 
-func (s *Service) Update(c context.Context, id int, p domain.UpdateEmployee) (*domain.Employee, error) {
+func (s *Service) Update(id int, p domain.UpdateEmployee) (*domain.Employee, error) {
 	args := s.Called(id, p)
 	return args.Get(0).(*domain.Employee), args.Error(1)
 }
 
-func (s *Service) Delete(c context.Context, id int) error {
+func (s *Service) Delete(id int) error {
 	args := s.Called(id)
 	return args.Error(0)
+}
+
+func (s *Service) CountInboundOrdersByAllEmployees() []domain.InboundOrdersByEmployee {
+	args := s.Called()
+	return args.Get(0).([]domain.InboundOrdersByEmployee)
+}
+
+func (s *Service) CountInboundOrdersByEmployee(id int) (*domain.InboundOrdersByEmployee, error) {
+	args := s.Called()
+	return args.Get(0).(*domain.InboundOrdersByEmployee), args.Error(1)
 }
