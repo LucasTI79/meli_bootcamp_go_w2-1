fd120bce3eb01676d92a597d57a72bd859a71f6e
Rafael Bonin <rafalyf11@gmail.com>
2023-07-10 09:58:57 -0300

    feat: added date formatter

--- pkg/helpers/helpers.go
+++ pkg/helpers/helpers.go
@@ -2,6 +2,7 @@ package helpers
 
 import (
 	"reflect"
+	"time"
 
 	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
@@ -19,4 +20,18 @@ func Fill(first interface{}, second interface{}) interface{} {
 func ToFormattedAddress(address string) string {
 	caser := cases.Title(language.BrazilianPortuguese)
 	return caser.String(address)
+}
+
+func ToDateTime(datetime string) time.Time {
+	dt, err := time.Parse("2006-01-02 15:04:05", datetime)
+
+	if err != nil {
+		panic("could not parse datetime")
+	}
+
+	return dt
+}
+
+func ToFormattedDateTime(datetime time.Time) string {
+	return datetime.Format("2006-01-02 15:04:05")
 }
