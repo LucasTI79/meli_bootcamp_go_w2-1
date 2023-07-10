dc3494cf2104e93f09ca31bd1c738703752260af
Rafael Bonin <rafalyf11@gmail.com>
2023-07-10 10:10:59 -0300

    feat: inbound orders by employee struct

--- internal/domain/employee.go
+++ internal/domain/employee.go
@@ -12,6 +12,15 @@ type Employee struct {
 	WarehouseID  int    `json:"warehouse_id"`
 }
 
+type InboundOrdersByEmployee struct {
+	ID           int    `json:"id"`
+	CardNumberID string `json:"card_number_id"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+	WarehouseID  int    `json:"warehouse_id"`
+	InboundOrdersCount int `json:"inbound_orders_count"`
+}
+
 type UpdateEmployee struct {
 	ID           *int    `json:"id"`
 	CardNumberID *string `json:"card_number_id"`
