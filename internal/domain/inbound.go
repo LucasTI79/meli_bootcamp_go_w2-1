4e4c3b2c4254029c23743eeafeee97811cc6f5a8
Rafael Bonin <rafalyf11@gmail.com>
2023-07-13 11:50:53 -0300

    fix: order number string

--- internal/domain/inbound.go
+++ internal/domain/inbound.go
@@ -5,7 +5,7 @@ import "time"
 type InboundOrder struct {
 	ID int `json:"id"`
 	OrderDate time.Time `json:"order_date"`
-	OrderNumber int `json:"order_number"`
+	OrderNumber string `json:"order_number"`
 	EmployeeId int `json:"employee_id"`
 	ProductBatchId int `json:"product_batch_id"`
 	WarehouseId int `json:"warehouse_id"`
