04dbfc3a636a47bcfbf9da873e0ff72fadbe0e18
oaugustocezar <augusto.c.ferreira@hotmail.com>
2023-07-13 14:42:06 -0300

    adjust coments from pull request

--- cmd/server/handler/carrier.go
+++ cmd/server/handler/carrier.go
@@ -4,6 +4,7 @@ import (
 	"github.com/extmatperez/meli_bootcamp_go_w2-1/internal/carrier"
 	"github.com/extmatperez/meli_bootcamp_go_w2-1/internal/domain"
 	"github.com/extmatperez/meli_bootcamp_go_w2-1/pkg/apperr"
+	"github.com/extmatperez/meli_bootcamp_go_w2-1/pkg/helpers"
 	"github.com/extmatperez/meli_bootcamp_go_w2-1/pkg/web"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -13,7 +14,7 @@ type CreateCarrierRequest struct {
 	CID         *string `json:"cid" binding:"required"`
 	CompanyName *string `json:"company_name" binding:"required"`
 	Address     *string `json:"address" binding:"required"`
-	Telephone   *int    `json:"telephone" binding:"required"`
+	Telephone   *string `json:"telephone" binding:"required,e164"`
 	LocalityID  *int    `json:"locality_id" binding:"required"`
 }
 
@@ -21,7 +22,7 @@ func (c CreateCarrierRequest) ToCarrier() domain.Carrier {
 	return domain.Carrier{
 		CID:         *c.CID,
 		CompanyName: *c.CompanyName,
-		Address:     *c.Address,
+		Address:     helpers.ToFormattedAddress(*c.Address),
 		Telephone:   *c.Telephone,
 		LocalityID:  *c.LocalityID,
 	}
