1c5632141dcd5ca6bd07ebfbb4b1d4c1d657bc5b
Rafael Bonin <rafalyf11@gmail.com>
2023-07-14 12:39:06 -0300

    fix: carrier service create tests

--- internal/carrier/service_test.go
+++ internal/carrier/service_test.go
@@ -1,13 +1,14 @@
 package carrier_test
 
 import (
+	"testing"
+
 	"github.com/extmatperez/meli_bootcamp_go_w2-1/internal/carrier"
 	"github.com/extmatperez/meli_bootcamp_go_w2-1/internal/carrier/mocks"
 	"github.com/extmatperez/meli_bootcamp_go_w2-1/internal/domain"
 	localityMocks "github.com/extmatperez/meli_bootcamp_go_w2-1/internal/locality/mocks"
 	"github.com/extmatperez/meli_bootcamp_go_w2-1/pkg/apperr"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 var (
@@ -52,6 +53,22 @@ func TestServiceCreate(t *testing.T) {
 		assert.Error(t, err)
 		assert.Nil(t, result)
 		assert.True(t, apperr.Is[*apperr.ResourceAlreadyExists](err))
+	})
+
+	t.Run("should return dependent resource not found error", func(t *testing.T) {
+		service, repository, localityRepo := CreateService(t)
+
+		var emptyLocality *domain.Locality
+		id := 1
+		repository.On("Exists", c.CID).Return(false)
+		localityRepo.On("Get", c.LocalityID).Return(emptyLocality)
+		repository.On("Save", c).Return(id)
+		repository.On("Get", id).Return(&c)
+		result, err := service.Create(c)
+
+		assert.Error(t, err)
+		assert.Nil(t, result)
+		assert.True(t, apperr.Is[*apperr.DependentResourceNotFound](err))
 	})
 }
 
